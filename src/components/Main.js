d24766d5e7ccd0dd6f0e15c67fbc1635464ec59b
Victor Avila <victoravila1997@gmail.com>
2022-06-19 22:46:03 -0500

    Added add skill logic to skills component and delete button logic for each skill

--- src/components/Main.js
+++ src/components/Main.js
@@ -3,8 +3,43 @@ import Info from "./Info";
 import Education from "./Education";
 import Experience from "./Experience";
 import Skills from "./Skills";
+import uniqid from "uniqid"
 
 class Main extends Component {
+    constructor() {
+        super();
+
+        this.state = {
+            skill: {
+                text: '',
+                id: uniqid(),
+            },
+            skills: []
+        }
+    }
+
+    handleSkillAdd = () => {
+        this.setState({
+            skills: this.state.skills.concat(this.state.skill),
+            skill: {
+              text: '',
+              id: uniqid(),
+            },
+          });
+    }
+
+    handleSkillChange = (e) => {
+        this.setState({
+            skill: {
+              text: e.target.value,
+              id: this.state.skill.id,
+            },
+          });
+    }
+
+    handleSkillDelete = (e) => {
+        this.setState({skills: this.state.skills.filter((_, index) => index !== +e.target.dataset.id)})
+    }
 
     render() {
         return (
@@ -12,7 +47,7 @@ class Main extends Component {
                 <Info />
                 <Education />
                 <Experience />
-                <Skills />
+                <Skills skills={this.state.skills} text={this.state.skill.text} handleChange={this.handleSkillChange} addButtonClicked={this.handleSkillAdd} deleteButtonClicked={this.handleSkillDelete}/>
             </main>
             
         )
--- src/components/Skills.js
+++ src/components/Skills.js
@@ -4,11 +4,18 @@ class Skills extends Component {
 
     render() {
         return (
-            <div>
-                <label>
-                    Skill
-                    <input type="text"></input>
-                </label>
+            <div className="skills">
+                <h3>Skills</h3>
+                <ul>
+                {this.props.skills.map((skill, index) => (
+                    <div key={skill.id} className="flex-container">
+                        <li>{skill.text}</li>
+                        <button data-id={index} onClick={this.props.deleteButtonClicked}>delete</button>
+                    </div>
+                ))}
+                </ul>
+                <input type="text" value={this.props.text} onChange={this.props.handleChange}></input>
+                <button className="add" onClick={this.props.addButtonClicked}>add</button>
             </div>
         )
     }
