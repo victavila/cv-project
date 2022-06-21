33115c8ebde7c8201c9687e21e33b97e40769fe8
Victor Avila <victoravila1997@gmail.com>
2022-06-20 20:05:16 -0500

    Added edit logic to skills component

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,3 +1,4 @@
+import { toHaveAccessibleDescription } from "@testing-library/jest-dom/dist/matchers";
 import React, { Component } from "react";
 import uniqid from "uniqid"
 
@@ -10,7 +11,8 @@ class Skills extends Component {
                 text: '',
                 id: uniqid(),
             },
-            skills: []
+            skills: [],
+            edit: false,
         }
     }
 
@@ -37,20 +39,46 @@ class Skills extends Component {
         this.setState({skills: this.state.skills.filter((_, index) => index !== +e.target.dataset.id)})
     }
 
+    handleEditClicked = () => {
+        this.setState({
+            edit: !this.state.edit,
+        })
+    }
+    
+    handleEditChange = (e) => {
+        let newSkills = [...this.state.skills];
+        let newSkill = {...newSkills[+e.target.dataset.id]};
+        newSkill.text = e.target.value;
+        newSkills[+e.target.dataset.id] = newSkill;
+        this.setState({skills: newSkills});
+    }
+
     render() {
+        
         return (
             <div className="skills">
                 <h3>Skills</h3>
                 <ul>
                 {this.state.skills.map((skill, index) => (
                     <div key={skill.id} className="flex-container">
-                        <li>{skill.text}</li>
+                        {
+                            this.state.edit ?
+                            <div>
+                                <input type="text" data-id={index} value={this.state.skills[index].text} onChange={this.handleEditChange}></input>
+                            </div>:
+                            <li>{skill.text}</li>
+                        }
                         <button data-id={index} onClick={this.handleDeleteClicked}>delete</button>
                     </div>
                 ))}
                 </ul>
                 <input type="text" value={this.state.skill.text} onChange={this.handleChange}></input>
                 <button className="add" onClick={this.handleAddClicked}>add</button>
+                <button onClick={this.handleEditClicked}>{
+                    this.state.edit ?
+                    <span>submit</span>:
+                    <span>edit</span>
+                }</button>
             </div>
         )
     }
